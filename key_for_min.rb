83ca5f3cc0146e1285097161dec4af26dbd20957
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-24 11:10:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,8 +10,8 @@ def key_for_min_value(name_hash)
         if value < min_value
           min_value = value
           min_key = key
-        else 
-          nil
+        elsif name_hash == {}
+        nil
         end
       end
       min_key
