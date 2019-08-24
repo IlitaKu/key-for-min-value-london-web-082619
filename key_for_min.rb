d5e139a6905774a239fa9b20e41c9ed65d19e5e6
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-24 10:59:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil 
   name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
+      bindig.pry
       lowest_value = v 
       lowest_key = k 
     end 
