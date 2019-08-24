0bda8ec1756ef0de7df4c95c2b9157672675793b
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-24 10:53:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
  min_key = name_hash.first[0]
  name_hash.each do |item, value|
    binding.pry
-   if value < min_value
+   if value > min_value
      min_value = value
      min_key = item
 end
